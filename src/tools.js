0de6f6bcbdb5520101c513caa12b54a70a88291c
Anders <palantus@gmail.com>
2020-10-23 22:45:47 +0200

    Fix channel

--- src/tools.js
+++ src/tools.js
@@ -1,12 +1,12 @@
-function cleanup(string){
+function cleanup(string, replacementChar = "-"){
     if(string)
-        return string.replace(/[^a-zA-Z0-9]/g, '-');
+        return string.replace(/[^a-zA-Z0-9_]/g, replacementChar);
     return string
 }
 
 function cleanupChannelSearch(string){
     if(string)
-        return string.replace(/[^a-zA-Z0-9\^\/]/g, "-");
+        return string.replace(/[^a-zA-Z0-9_\^\/]/g, "-");
     return string
 }
 
